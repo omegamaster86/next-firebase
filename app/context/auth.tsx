bbfbcef4dad213339dbaf0f6006300a4a12da512
omegamaster86 <masya868686@gmail.com>
2024-09-01 20:34:54 +0900

    認証の中身を作成

--- app/context/auth.tsx
+++ app/context/auth.tsx
@@ -1,13 +1,55 @@
-import { createContext } from "react";
+import { createContext,type ReactNode, useEffect, useState } from "react";
 import type { User } from "../types/user";
-
-// コンテクスト用の型を定義
-// 最終的な値が null だけだと、ログインしていないから空なのか、あるいはまだ状態を参照中（ローディング中）なのかの区別がつきません。
-// 初期値を undefined とし、未ログインの場合に null を代入することで、 
-// undefined であればローディング中のUIを表示し、 
-// null なら「ログインしてください」というUIを表示する。といった切り分けが可能になります。
+import { onAuthStateChanged } from '@firebase/auth';
+import { doc, getDoc, setDoc } from '@firebase/firestore';
+import { auth, db } from '../firebase/client';
 
 type UserContextType = User | null | undefined;
 
 // コンテクストを作成
 const AuthContext = createContext<UserContextType>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  // 配布したいデータの定義
+  const [user, setUser] = useState<UserContextType>();
+
+  useEffect(() => {
+    // ログイン状態を監視し、変化があったら発動
+    const unsubscribe = onAuthStateChanged(auth, async (firebaseUser) => {
+      if (firebaseUser) {
+        // ログインしていた場合、ユーザーコレクションからユーザーデータを参照
+        const ref = doc(db, `users/${firebaseUser.uid}`);
+        const snap = await getDoc(ref);
+
+        if (snap.exists()) {
+          // ユーザーデータを取得して格納
+          const appUser = (await getDoc(ref)).data() as User;
+          setUser(appUser);
+        } else {
+          // ユーザーが未作成の場合、新規作成して格納
+          const appUser: User = {
+            id: firebaseUser.uid,
+            name: firebaseUser.displayName ?? '',
+            photoURL: firebaseUser.photoURL ?? '',
+            email: firebaseUser.email ?? '',
+            createdAt: Date.now(),
+          };
+
+          // Firestoreにユーザーデータを保存
+          setDoc(ref, appUser).then(() => {
+            // 保存に成功したらコンテクストにユーザーデータを格納
+            setUser(appUser);
+          });
+        }
+      } else {
+        // ログインしていない場合、ユーザー情報を空にする
+        setUser(null);
+      }
+      // このコンポーネントが不要になったら監視を終了する
+      return unsubscribe;
+    });
+  }, []);
+
+  // プロバイダーを作成し、配布物を格納する
+  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
+};
